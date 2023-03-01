402f89a70dfdf0aadd86de3a5427359f1ddcd10f
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-03-01 15:50:45 +0900

    Fix: model index.tsx

--- src/model/index.tsx
+++ src/model/index.tsx
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './User/loginSlice';
+import signupReducer from './User/signupSlice';
+import memberDataReducer from './User/memberDataSlice';
 
 export const store = configureStore({
     reducer: {
-        login: loginReducer
+        login: loginReducer,
+        singup: signupReducer,
+        memberData: memberDataReducer,
     }
 });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
