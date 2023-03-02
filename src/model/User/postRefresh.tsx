bff97efe4e3faa7f11058601aa61e72a8b5b4dfd
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-03-02 20:19:32 +0900

    Fix: postRefresh.tsx

--- src/model/User/postRefresh.tsx
+++ src/model/User/postRefresh.tsx
@@ -4,7 +4,7 @@ import axios, { AxiosResponse } from 'axios';
 import { loginFailure, loginSuccess } from "./slice/loginSlice";
 
 const postRefresh = async () => {
-    const refresh = useSelector((state: RootState) => state.login.refreshToken);
+    //const refresh = useSelector((state: RootState) => state.login.refreshToken);
     const access = useSelector((state: RootState) => state.login.accessToken);
 
     const dispatch = useDispatch();
@@ -12,7 +12,7 @@ const postRefresh = async () => {
     try {
         const res: AxiosResponse = await axios.post("http://25.15.132.100:8080/api/refershToken",
             {
-                refreshToken: refresh,
+                //refreshToken: refresh,
                 accessToken: access
             },//여기가 json형태로 바꾸든, 아니면 헤더에 넣든 해야될듯...
             {
