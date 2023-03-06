5966f7ffd8f2e76b208cce3a28fe5c51fa4c3243
CsLsj <lsj001018@naver.com>
2023-03-06 14:18:39 +0900

    add profile page

---
+++ src/components/AnimalModalComponent.tsx
@@ -0,0 +1,136 @@
+import React, {useCallback, useState} from 'react';
+import {Button , Pressable, StyleSheet, Text, TextInput, View, Image} from 'react-native';
+import Modal from 'react-native-modal';
+import Picture from './PictureComponent';
+
+interface IModal{
+    show : boolean;
+    handleClose : () => void;
+}
+
+function AnimalModal({show, handleClose} : IModal){
+    const [name, setName] = useState('');
+    const [birth, setBirth] = useState('');
+    const [gender, setGender] = useState('');
+    const [breed, setBreed] = useState('');
+    const [weight, setWeight] = useState('');
+    let image = {
+        imageUrl : "",
+        imageName : "",
+    }
+
+    const onChangeName = useCallback((text:string) => {
+        setName(text.trim())
+    }, []);
+    const onChangeBirth = useCallback((text:string) =>{
+        setBirth(text.trim())
+    }, []);
+    const onChangeGender = useCallback((text:string) => {
+        setGender(text.trim())
+    }, []);
+    const onChangeBreed = useCallback((text:string) => {
+        setBreed(text.trim())
+    }, []);
+    const onChangeWeight = useCallback((text:string) => {
+        setWeight(text.trim())
+    }, []);
+
+    const [visible, setVisible] = useState(false);
+    const handleSave = () => {
+        handleClose();
+    }
+    return(
+        <Modal
+            animationIn = "slideInUp"
+            animationOut= "slideOutDown"
+            isVisible = {show}
+            onBackdropPress={() => handleClose()}
+            onModalHide={handleClose}
+            backdropColor= 'gray'>
+            <View style = {styles.Modal}>
+               <View style = {styles.wrapper}>
+                 <Text style = {styles.text}>사진</Text>
+                 <Pressable
+                    onPress = {Picture}/>
+              </View>
+              <View style = {styles.wrapper}>
+                <Text style = {styles.text}>이름</Text>
+                  <TextInput
+                    style = {styles.textInput}
+                    value = {name}
+                    placeholder = "이름"
+                    placeholderTextColor= "#666"
+                    onChangeText={onChangeName}
+                    clearButtonMode= "while-editing"
+                    blurOnSubmit = {false}/>
+              </View>
+              <View style = {styles.wrapper}>
+                <Text style = {styles.text}>생일</Text>
+                <TextInput
+                    style = {styles.textInput}
+                    value = {birth}
+                    placeholder = "생일"
+                    placeholderTextColor= "#666"
+                    onChangeText={onChangeBirth}
+                    clearButtonMode= "while-editing"
+                    blurOnSubmit = {false}/>
+              </View>
+              <View style = {styles.wrapper}>
+                <Text style = {styles.text}>성별</Text>
+                <TextInput
+                    style = {styles.textInput}
+                    value = {gender}
+                    placeholder = "성별"
+                    placeholderTextColor= "#666"
+                    onChangeText={onChangeGender}
+                    clearButtonMode= "while-editing"
+                    blurOnSubmit = {false}/>
+              </View>
+              <View style = {styles.wrapper}>
+                <Text style = {styles.text}>견종</Text>
+                <TextInput
+                    style = {styles.textInput}
+                    value = {breed}
+                    placeholder = "견종"
+                    placeholderTextColor= "#666"
+                    onChangeText={onChangeBreed}
+                    clearButtonMode= "while-editing"
+                    blurOnSubmit = {false}/>
+              </View>
+              <View style = {styles.wrapper}>
+                <Text style = {styles.text}>몸무게</Text>
+                <TextInput
+                    style = {styles.textInput}
+                    value = {weight}
+                    placeholder = "몸무게"
+                    placeholderTextColor= "#666"
+                    onChangeText={onChangeWeight}
+                    clearButtonMode= "while-editing"
+                    blurOnSubmit = {false}/>
+              </View>
+            </View>
+
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    Modal : {
+        backgroundColor : "white",
+        borderRadius : 20,
+    },
+    wrapper : {
+        flexDirection : "row",
+        paddingLeft : 10,
+        marginVertical : 5,
+    },
+    text : {
+        marginTop : 14,
+        borderBottomWidth : StyleSheet.hairlineWidth,
+    },
+    textInput : {
+        marginLeft : 10,
+    }
+})
+
+export default AnimalModal;
--- src/components/PictureComponent.tsx
+++ src/components/PictureComponent.tsx
@@ -5,74 +5,85 @@ import {Alert} from 'react-native';
 
 
 function Picture(){
-  const image={
-    imageUrl:"",
-    imageName:"",
-  }
-    const androidPermission = async () => {
-        const allowCamera = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.CAMERA,
-            {
-                title : "Camera App Permission",
-                message : "Camera App needs access to your camera",
-                buttonNeutral : "Ask me Later",
-                buttonNegative : "Cancel",
-                buttonPositive : "Ok",
-            }
-        );
-        const allowStorage = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            {
-                title : "Camera App Permission",
-                message : "Camera App needs access to your camera",
-                buttonNeutral : "Ask me Later",
-                buttonNegative : "Cancel",
-                buttonPositive : "Ok",
-            }
-        );
-        if(allowCamera === PermissionsAndroid.RESULTS.GRANTED && allowStorage === PermissionsAndroid.RESULTS.GRANTED){
-            console.log("접근 허용");
-        }
-        else{
-            console.log("접근 거절");
-        }
+  let image= {
+    imageUrl: "",
+    imageName: "",
+  };
+
+  const androidPermission = async () => {
+    const allowCamera = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title : "Camera App Permission",
+        message : "Camera App needs access to your camera",
+        buttonNeutral : "Ask me Later",
+        buttonNegative : "Cancel",
+        buttonPositive : "Ok",
+      }
+    );
+    const allowStorage = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      {
+        title : "Camera App Permission",
+        message : "Camera App needs access to your camera",
+        buttonNeutral : "Ask me Later",
+        buttonNegative : "Cancel",
+        buttonPositive : "Ok",
+      }
+    );
+    if(allowCamera === PermissionsAndroid.RESULTS.GRANTED && allowStorage === PermissionsAndroid.RESULTS.GRANTED){
+        console.log("접근 허용");
     }
-    Alert.alert(
-        "무엇으로 찍으실 건가요?",
-        "옵션을 선택해주세요",
-        [
-          {
-            text: "카메라로 찍기",
-            onPress: async() =>{
-              const result = await launchCamera({
-                mediaType : 'photo', 
-                cameraType : 'back',
-                saveToPhotos : true, 
-              });
-                if (result.didCancel){ 
-                  return null;
-                }
-                const imageUrl = result.assets[0].uri;
-                const uriPath = imageUrl.split("//").pop();
-                const imageName = imageUrl.split("/").pop();
+    else{
+        console.log("접근 거절");
+    }
+  }
+
+  Alert.alert(
+    "무엇으로 찍으실 건가요?",
+    "옵션을 선택해주세요",
+    [
+      {
+        text: "카메라로 찍기",
+        onPress: async() =>{
+          const result = await launchCamera({
+            mediaType : 'photo', 
+            cameraType : 'back',
+            saveToPhotos : true, 
+          });
+            if (result.didCancel){ 
+              return null;
             }
-          },
-          {
-            text: "앨범에서 선택",
-            onPress: async() =>{
-              const result = await launchImageLibrary();
-              if (result.didCancel){
-                return null;
-              } 
-              const imageUrl = result.assets[0].uri;
-              const uriPath = imageUrl.split("//").pop();
-              const imageName = imageUrl.split("/").pop();
-            }
-          },
-        ],
-        {cancelable: false}
-      );
-      return image
+            const tempUrl = result.assets[0].uri;
+            image.imageUrl = tempUrl;
+            image.imageName = tempUrl.split("/").pop();
+            return image.imageUrl
+        }
+      },
+      {
+        text: "앨범에서 선택",
+        onPress: async() =>{
+          const result = await launchImageLibrary();
+          if (result.didCancel){
+            return null;
+          } 
+          const tempUrl = result.assets[0].uri;
+          image.imageUrl = tempUrl;
+          image.imageName = tempUrl.split("/").pop();
+          return image.imageUrl
+        }
+      },
+    ],
+    {cancelable: false}
+  );
+
+  console.log("여기");
+  console.log(image.imageUrl);
 }
 
+function onPressEventListener() {
+
+
+return Image;
+}
 export default Picture;
