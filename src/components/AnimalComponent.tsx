877c1741a3cf9bc25a8eeabb20229e840b55ecca
CsLsj <lsj001018@naver.com>
2023-03-06 17:13:02 +0900

    fix animalComponent

--- src/components/AnimalComponent.tsx
+++ src/components/AnimalComponent.tsx
@@ -8,56 +8,38 @@ import {
   View,
   ScrollView,
   Button,
+  Image
 } from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import { RootStackParamList } from '../AppInner';
 import AnimalModal from './AnimalModalComponent';
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown'
 
 type AnimalScreenProps = NativeStackScreenProps<RootStackParamList, 'Animal'>;
 
 
 function Animal({navigation}: AnimalScreenProps) {
-  const [modalVisible, setModalVisible] = useState(false);
-  const showModal = () => {
-    setModalVisible(true);
-  }
-
+  const [breed, setBreed] = useState(null);
   return (
-    <ScrollView style = {styles.AnimalAddPage}>
-      <View style = {styles.AnimalAddZone}>
-        <Text style = {styles.AnimalAddText}>등록된 애완견이 없습니다</Text>
-        <Text style = {styles.AnimalAddText}>애완견을 추가해 주세요</Text>
-      </View>
-      <View  style = {styles.AddButtonZone}>
-        <Button title = "추가하기" onPress = {showModal}/>
-        <AnimalModal show = {modalVisible}
-         handleClose={() => setModalVisible(false)}/>
-      </View>
-    </ScrollView>
-  );
+    <View style = {styles.wrapper}>
+      <Text>견종</Text>
+      <AutocompleteDropdown
+        clearOnFocus={false}
+        closeOnBlur={true}
+        closeOnSubmit={false}
+        onSelectItem={setBreed}
+        dataSet={[
+          {id : 1, title : ''},
+          {id : 2, title : ''},
+        ]}/>
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  AnimalAddPage : {
-    backgroundColor : 'snow',
-    flex : 1,
-  },
-  AnimalAddZone : {
-    marginTop : 30,
-    alignItems : "center",
-    flex : 1,
-  },
-  AnimalAddText : {
-    fontSize : 16,
-    fontWeight : "900"
-  },
-  AddButtonZone : {
-    marginTop : 50,
-    alignItems : "center",
-    justifyContent : "center",
-  },
-  AddButton : {
-    fontSize : 20,
+  wrapper : {
+    flexDirection : 'row',
   },
 })
 
