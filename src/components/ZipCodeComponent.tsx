3046f8b8d71502edd4e130e00ae0775d808d980b
CsLsj <lsj001018@naver.com>
2023-03-09 18:00:43 +0900

    fix ZipCode Component

--- src/components/ZipCodeComponent.tsx
+++ src/components/ZipCodeComponent.tsx
@@ -1,38 +1,68 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import {View, StyleSheet, Text, Modal, Pressable} from 'react-native';
 import Postcode from '@actbase/react-daum-postcode';
 
 
-function ZipCode(){
+interface AddressProps{
+    onAddressSelected : (address : {
+        zonecode : string,
+        street : string;
+    }) => void;
+}
+function ZipCode({onAddressSelected} : AddressProps){
+    const [modal, setModal] = useState(false);
     const getAddressData = (data : any) => {
-        let defaultAddress = '';
-        if (data.buildingName === 'N') {
-          defaultAddress = data.apartment;
-        } else {
-          defaultAddress = data.buildingName;
-        } 
+        let zonecode = '';
+        let address = '';
+        let buildingName = '';
+        if (data.zonecode) {
+            zonecode = data.zonecode;
+          }
+          if (data.address) {
+            address = data.address;
+          }
+          if (data.buildingName) {
+            buildingName = data.buildingName;
+          }
+          const street = `${address}${buildingName ? ` (${buildingName})` : ''}`;
+          onAddressSelected({zonecode, street});
+          setModal(false);
     }
 
-
     return(
-        <View>
-            <Postcode
-            style={{ flex: 1, width: '100%', zIndex: 999 }}
-            jsOptions={{ animation: true }}
-            onSelected={data => getAddressData(data)}
-            onError={function (error: unknown): void {
-            throw new Error('Function not implemented.');
-            }}
-            />
+        <View style = {styles.zipCode}>
+            <Pressable 
+              style = {styles.zipCodeZone}
+              onPress = {() => setModal(true)}>
+            <Text>검색</Text>
+            </Pressable>
+            <Modal 
+              visible = {modal}
+              animationType= "slide">
+                <Postcode
+                style = {styles.form}
+                jsOptions={{ animation: true}}
+                onSelected={data => getAddressData(data)}
+                onError={function (error: unknown): void {
+                throw new Error('Function not implemented.');
+                }}/>
+            </Modal>
         </View>
     )
 }
 
-const stlyes = StyleSheet.create({
+const styles = StyleSheet.create({
+    zipCode : {
+        marginRight : 322,
+    },
+    zipCodeZone : {
+        backgroundColor : 'gray',
+        borderRadius : 5,
+    },
     form : {
         width : 320,
         height : 320,
-    }
+    },
 })
 
 export default ZipCode;
