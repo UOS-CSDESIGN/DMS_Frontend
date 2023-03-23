f6416abad9f2a5050fb4efa51406369924b4c6ed
CsLsj <lsj001018@naver.com>
2023-03-23 13:06:15 +0900

    add etc variable in genderComponent

--- src/components/genderComponent.tsx
+++ src/components/genderComponent.tsx
@@ -7,21 +7,36 @@ type GenderProps = {
 }
 
 function GenderComponent({onGenderChange} : GenderProps){
+    const [etc, setEtc] = useState(false);
     const [male, setMale] = useState(false);
     const [female, setFemale] = useState(false);
+    const onChangeEtc = useCallback(() => {
+        setEtc(true);
+        setMale(false);
+        setFemale(false);
+        onGenderChange(0);
+    }, [])
     const onChangeMale = useCallback(() => {
         setMale(true);
         setFemale(false);
+        setEtc(false);
         onGenderChange(1);
     }, [])
     const onChangeFemale = useCallback(() => {
         setFemale(true);
         setMale(false);
+        setEtc(false);
         onGenderChange(2);
     }, [])
     
     return(
         <View style = {styles.genderBox}>
+            <CheckBox
+                disabled = {false}
+                value = {etc}
+                onValueChange = {onChangeEtc}
+            />
+            <Text style = {styles.gender}>기타</Text>
             <CheckBox
                 disabled = {false}
                 value = {male}
