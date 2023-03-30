3abcf69d2f84a3bb2c6373865a9383d909f667a5
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-03-30 20:43:30 +0900

    Fix: google social login of modifiy already exist user

--- src/model/User/getGoogleSignin.tsx
+++ src/model/User/getGoogleSignin.tsx
@@ -2,21 +2,21 @@ import { User } from "@react-native-google-signin/google-signin";
 import axios, { AxiosError } from "axios";
 import getMemberData from "./getMemberData";
 import { loginFailure, loginRequest, loginSuccess } from "./slice/loginSlice";
-import { RootStackParamList } from "../../AppInner";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useState } from "react";
 import Config from "react-native-config";
+
 /**
  * @param userInfo : google social infomation
  * @param dispatch : login redux dispatcher
  * @param user : user store
  * @param token : access toek store
- * @returns true-no more info needed false-get more info needed
+ * @returns 200: already exist mail, 201: default goolge social, 202: need more info
  */
+
 //get accessToken by passing id_Token and checking if needs signup.
-const getGoogleSignin = async (userInfo: User, dispatch: any, user:any, token:string) => {
+const getGoogleSignin = async (userInfo: User, dispatch: any, user:any) => {
 
     dispatch(loginRequest());
+
     console.log(`${Config.SPRING_API}/api/oauth2/google?id_token=${userInfo.idToken}`);
     return await axios.get(`${Config.SPRING_API}/api/oauth2/google?id_token=${userInfo.idToken}`,
         {
@@ -28,13 +28,23 @@ const getGoogleSignin = async (userInfo: User, dispatch: any, user:any, token:st
         .then((res) => {
             dispatch(loginSuccess(JSON.stringify(res.data.accessToken)));
             getMemberData(dispatch, JSON.stringify(res.data.accessToken));
-            //add implement of signup
-            //add more condition for not signuped
-            if (user.addressDetail === "") {
-                console.log('is here');
-                return false;
-            }else{
-                return true;
+            //status 201 : default google social
+            //status 202 : successful google social
+            if (res.status === 201) {
+                if (user.addressDetail === "") {
+                    //not signed
+                    console.log('is here');
+                    return 201;
+                } else {
+                    //signed
+                    return 202;
+                }
+            }
+            //status 200 : gmail already exist
+            else if (res.status === 200) {
+                //alert
+                //link to google social 
+                return 200;
             }
         })
         .catch((error:AxiosError) => {
