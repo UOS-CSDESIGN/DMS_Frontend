e689c858acc2c38c9f905210bb171356bbfc6b9d
CsLsj <lsj001018@naver.com>
2023-04-07 21:11:46 +0900

    fix NonSocialMyPageComponent

--- src/components/NonSocialMyPageComponent.tsx
+++ src/components/NonSocialMyPageComponent.tsx
@@ -14,7 +14,6 @@ import {
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import { RootStackParamList } from '../AppInner';
 import MyPage from './MyPageComponent';
-import { MyPageProps } from './MyPageComponent';
 import getMemberData from '../model/User/getMemberData';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../model';
@@ -22,10 +21,12 @@ import { RootState } from '../model';
 type NonSocialMyPageScreenProps  = NativeStackScreenProps<RootStackParamList, 'NonSocialMyPage'>
 
 function NonSocialMyPage({navigation} : NonSocialMyPageScreenProps){
-    const [myPageProps, setMyPageProps] = useState<MyPageProps>({
+    const [myPageProps, setMyPageProps] = useState({
         name: '',
         nickname: '',
         password: '',
+        passwordConfirm : '',
+        isSocial : false,
         gender: '',
         birth: '',
         email: '',
@@ -36,41 +37,174 @@ function NonSocialMyPage({navigation} : NonSocialMyPageScreenProps){
         addressDetail: '',
         picture: '',
     });
-    const dispatch=useDispatch();
+    const [editedMyPageProps, setEditedMyPageProps] = useState({
+      name : '',
+      nickname : '',
+      password : '',
+      passwordConfirm : '',
+      isSocial : false,
+      gender : '',
+      birth : '',
+      email : '',
+      editEmail : false,
+      phoneNo : '',
+      zipcode : '',
+      street : '',
+      addressDetail : '',
+      picture : '',
+    });
+    const onChangeName = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, name : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, name : text}));
+    }
+    const onChangeNickname = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, nickname : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, nickname : text}));
+    }
+    const onChangePassword = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, password : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, password : text}));
+    }
+    const onChangePasswordConfirm = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, passwordConfirm : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, passwordConfirm : text}));
+    }
+    const onChangeGender = ((text : number) => {
+      const gender = text === 1 ? "남성" : "여성";
+      setMyPageProps((prevState) => ({ ...prevState, gender : gender}));
+      console.log(myPageProps.gender);
+      setEditedMyPageProps((prevState) => ({ ...prevState, gender : gender}));
+    })
+    const onChangeBirth = ((text : string) => {
+      setMyPageProps((prevState) => ({...prevState, birth : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, birth : text}))
+    })
+    const onChangeEmail = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, email : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, email : text}));
+    }
+    const onChangePhoneNo = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, phoneNo : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, phoneNo : text}));
+    }
+    const onChangeZipcode = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, zipcode : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, zipcode : text}));
+    }
+    const onChangeStreet = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, street : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, street : text}));
+    }
+    const onChangeaddressDetail = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, addressDetail : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, addressDetail : text}));
+    }
+    const onChangePicture = (text : string) => {
+      setMyPageProps((prevState) => ({...prevState, picture : text}));
+      setEditedMyPageProps((prevState) => ({...prevState, picture : text}));
+    }
+
+    const dispatch = useDispatch();
+
+    const onSocialLogin = useCallback(() => {
+
+    }, [])
     const token = useSelector((state:RootState)=>state.login.accessToken);
     const userData = useSelector((state:RootState)=>state.memberData.userData);
+    const onSubmit = useCallback(() => {
+
+    }, []);
     useEffect(() => {
       getMemberData(dispatch, token);
       setMyPageProps({
         name:userData.username,
         nickname:userData.nickname,
-        password:"",
-        gender:userData.gender,
+        password :"",
+        passwordConfirm : "",
+        isSocial : false,
+        gender:userData.gender === 1? "남성" : "여성",
         birth:userData.birth,
         email:userData.email,
         phoneNo:userData.phoneNo,
-        zipcode:userData.zipCode,
+        zipcode:userData.zipcode,
         street:userData.street,
         addressDetail:userData.addressDetail,
         picture:userData.imageUrl,
         editEmail : true,
       })
     },[])
-    return(
-        <MyPage
-          name = {myPageProps.name}
-          nickname = {myPageProps.nickname}
-          password = {myPageProps.password}
-          gender = {myPageProps.gender}
-          birth = {myPageProps.birth}
-          email = {myPageProps.email}
-          editEmail = {myPageProps.editEmail}
-          phoneNo = {myPageProps.phoneNo}
-          zipcode = {myPageProps.zipcode}
-          street = {myPageProps.street}
-          addressDetail={myPageProps.addressDetail}
-          picture = {myPageProps.picture}/>
-    )
+    if(true){
+      return(
+        <ScrollView>
+          <View>
+            <Pressable
+             style = {styles.socialLoginButtonZone}
+             onPress = {onSocialLogin}>
+              <Text style = {styles.buttonText}>소셜 로그인 연동</Text>
+             </Pressable>
+          </View>
+          <View>
+            <MyPage
+            name = {myPageProps.name}
+            onChangeName = {onChangeName}
+            nickname = {myPageProps.nickname}
+            onChangeNickname = {onChangeNickname}
+            password = {myPageProps.password}
+            onChangePassword = {onChangePassword}
+            passwordConfirm= {myPageProps.passwordConfirm}
+            onChangePasswordConfirm = {onChangePasswordConfirm}
+            gender = {myPageProps.gender}
+            onChangeGender = {onChangeGender}
+            birth = {myPageProps.birth}
+            onChangeBirth = {onChangeBirth}
+            isSocial = {myPageProps.isSocial}
+            email = {myPageProps.email}
+            onChangeEmail = {onChangeEmail}
+            phoneNo = {myPageProps.phoneNo}
+            onChangePhoneNo = {onChangePhoneNo}
+            zipcode = {myPageProps.zipcode}
+            onChangeZipcode = {onChangeZipcode}
+            street = {myPageProps.street}
+            onChangeStreet = {onChangeStreet}
+            addressDetail={myPageProps.addressDetail}
+            onChangeAddressDetail = {onChangeaddressDetail}
+            picture = {myPageProps.picture}
+            onChangePicture = {onChangePicture}/>
+          </View>
+          <View>
+            <Pressable
+             style = {styles.buttonZone}
+             onPress = {onSubmit}>
+              <Text style = {styles.buttonText}>수정하기</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      )
+    }
 }
+
+const styles = StyleSheet.create({
+  socialLoginButtonZone : {
+    backgroundColor : '#1C76E9',
+    justifyContent : 'center',
+    alignItems : 'center',
+    marginHorizontal : 100,
+    borderWidth : 1,
+    borderColor : "gray",
+    borderRadius : 5,
+  },
+  buttonZone : {
+    backgroundColor : '#1C76E9',
+    justifyContent : 'center',
+    alignItems : 'center',
+    marginHorizontal : 140,
+    borderWidth : 1,
+    borderColor : "gray",
+    borderRadius : 5,
+  },
+  buttonText : {
+    fontSize : 20,
+  }
+})
 
 export default NonSocialMyPage;
