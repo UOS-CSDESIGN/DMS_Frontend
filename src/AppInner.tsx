fb8561a04e77ccb45cf84c8bff79e9f9f70e07cb
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-05-22 22:26:08 +0900

    Fix: merge User, Pet, PetObestiy

--- src/AppInner.tsx
+++ src/AppInner.tsx
@@ -1,102 +1,49 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { useState, useEffect } from 'react';
-import LoginPage from './pages/loginPage';
-import SignUpPage from './pages/signUpPage';
+import {createDrawerNavigator, DrawerItem, DrawerToggleButton} from '@react-navigation/drawer'
+import {useState} from 'react';
+import Login from './components/LoginComponent';
+import SignUp from './components/SignUpComponent';
 import Main from './components/mainComponent';
-import SocialMyPage from './components/SocialMyPageComponent';
+import MyPage from './components/MyPageComponent';
 import Animal from './components/AnimalComponent';
 import SocialGoogle from './components/socialGoogle';
 import Find from './components/FindComponent';
 import MultiProfile from './components/MultiProfileComponent';
-import Album from './components/AlbumComponent';
-import NonSocialMyPage from './components/NonSocialMyPageComponent';
-import NonSocialWithdrawal from './components/NonSocialWithdrawalComponent';
-import SocialWithdrawal from './components/SocialWithdrawalComponent';
+import ObesityPage from './pages/obesityPage';
+import ObesityDetailPage from './pages/obesityDetailPage';
+import CommunityPage from './pages/communityPage';
+import { Alert, Pressable, Text, View } from 'react-native'
 
 
 export type RootStackParamList = {
-
-  LoginPage : undefined;
-  SignUpPage : undefined;
-  SocialMyPage : undefined;
-  NonSocialMyPage : undefined;
-  googleSignUp : undefined;
-
+  Login: undefined;
+  SignUp: undefined;
+  MyPage : undefined;
   Find : undefined;
   Animal : undefined;
   Main : undefined;
   SocialGoogle : undefined;
-
-  NonSocialWithdrawal : undefined;
-  SocialWithdrawal : undefined;
-
+  MultiProfile : undefined;
+  Shop : undefined;
+  ObesityPage : undefined;
+  MapPage : undefined;
+  Root : undefined;
+  ObesityDetailPage : undefined;
+  CommunityPage : undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const Drawer = createDrawerNavigator();
 
-function AppInner() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  
-  return (
-    <NavigationContainer>
-      {isLoggedIn ? (
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Main"
-            component={Main}
-            options={{title: '메인페이지'}}
-          />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="LoginPage"
-            component={LoginPage}
-            options={{title: '로그인'}}
-          />
-          <Stack.Screen
-            name="SignUpPage"
-            component={SignUpPage}
-            options={{title: '회원가입'}}
-          />
-          <Stack.Screen
-            name = "SocialMyPage"
-            component = {SocialMyPage}
-            options = {{title : '소셜마이페이지'}}/>
-          <Stack.Screen
-            name = "NonSocialMyPage"
-            component = {NonSocialMyPage}
-            options = {{title : '비소셜마이페이지'}}/>
-          <Stack.Screen
-            name = "Animal"
-            component = {Animal}
-            options = {{title : '애완견'}}
-            />
-          <Stack.Screen
-            name = "Main"
-            component = {Main}
-            options = {{title : '메인페이지'}}/>
-          <Stack.Screen
-            name = "SocialGoogle"
-            component = {SocialGoogle}
-            options = {{title : '소셜로그인'}}/>
-          <Stack.Screen
-            name = "NonSocialWithdrawal"
-            component = {NonSocialWithdrawal}
-            options = {{title : '비소셜회원탈퇴'}}/>
-          <Stack.Screen
-            name = "SocialWithdrawal"
-            component = {SocialWithdrawal}
-            options = {{title : '소셜회원탈퇴'}}/>
-        </Stack.Navigator>
-        
-      )}
-    </NavigationContainer>
-  );
+const optionDrawer = {
+  headerLeft : () => null,
+  headerRight : () => <DrawerToggleButton/>,
+  headerBackVisible : true,
+}
+const optionStack = {
+  headerShown : false
 }
 
 /* function CustomDrawerContent(props : any) {
