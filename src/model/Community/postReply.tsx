15b3d9f232be50516f8cf9e6a61fe3a96ccbf97a
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-06-02 14:34:14 +0900

    feat:post reply

--- src/model/Community/postReply.tsx
+++ src/model/Community/postReply.tsx
@@ -2,7 +2,7 @@ import PostReply from './PostReply';
 import COnfig from 'react-native-config';
 import axios from 'axios';
 
-async function postReply (reply:PostReply, token:string, dispatch:any){
+async function postReply (reply:PostReply, token:string, dispatch:any):Promise<any>{
 
     const bearer = `Bearer ${JSON.parse(token)}`;
     const url = `${COnfig.SPRING_API}/`;
