7e34523ca6a2496d4d7c61b9da52e1ccf3245918
CsLsj <lsj001018@naver.com>
2023-06-02 14:10:03 +0900

    fix ObesityComponent

--- src/components/ObesityComponent.tsx
+++ src/components/ObesityComponent.tsx
@@ -8,82 +8,51 @@ import { Dimensions } from "react-native";
 
 
 interface WeightProps {
+    picture : string;
     goal : string;
     tempGoal : string;
     weight : string;
-    tempWeight : string;
     obesity : number;
-    isEditWeight : boolean;
     isEditGoal : boolean;
-    onChangeWeight : (event : GestureResponderEvent) => void;
-    onChangeWeightText : (text : string) => void;
+    toObesityRegister : (event : GestureResponderEvent) => void;
     onChangeGoal : (event : GestureResponderEvent) => void;
     onChangeGoalText : (text : string) => void;
-    onSaveWeight : (event : NativeSyntheticEvent<TextInputFocusEventData>) => void;
     onSaveGoal : (event : NativeSyntheticEvent<TextInputFocusEventData>) => void;
     }
 
 function Obesity(props : WeightProps){   
-    const [noticeVisible, setNoticeVisible] = useState<boolean>(false);
-    const showNoticeModal = useCallback(() => {
-        setNoticeVisible(true);
-    },[])
-    
-    const [picture, setPicture] = useState<string>('');
-    //사진 받아오는 코드
-    const [obesity, setObesity] = useState<number>(0);
-    //비만도 받아오는 코드
-    const [pictureVisible, setPictureVisible] = useState<boolean>(false);
-    const showPictureModal = useCallback(() => {
-        setPictureVisible(true);
-    }, [])
     return(
         <View style = {styles.View}>
             <View style = {styles.PictureWrapper}>
-                {picture ? <Text>비만도 : {obesity}</Text>
+                {props.picture ? <Text>비만도 : {props.obesity}</Text>
                 : <View>
                     <Pressable
                      style = {styles.plusButton}
-                     onPress = {showPictureModal}>
+                     onPress = {props.toObesityRegister}>
                         <FontAwesomeIcon icon={faPlusCircle} size = {20} color = "#e4bd12"/>
                     </Pressable>
                     <View style = {styles.pictureTextWrapper}>
                         <Text style = {styles.text}>등록된 비만도가 없습니다</Text>
-                        <Text style = {styles.text}>사진을 등록해주세요</Text>
+                        <Text style = {styles.text}>비만도 정보를 등록해주세요</Text>
                     </View>
                   </View>}
             </View>
             <View style = {styles.GoalWrapper}>
                 <View style = {styles.GoalInnerWrapper1}>
                     <Text style = {styles.text}>현재 몸무게</Text>
-                    {props.isEditWeight ? (
-                        <TextInput
-                        style = {styles.textInput}
-                        value = {props.tempWeight}
-                        onChangeText = {props.onChangeWeightText}
-                        onBlur = {props.onSaveWeight}
-                        keyboardType = 'numeric'/>
-                    ) : <Text style = {styles.textWeight}>{props.weight}</Text>}
-                    <Pressable
-                     onPress = {props.onChangeWeight}
-                     style = {styles.editButton}>
-                        <Text style = {styles.textButton}>수정</Text>
-                    </Pressable>
+                    <Text style = {styles.textWeight}>{props.weight}</Text>
                 </View>
                 <View style = {styles.GoalInnerWrapper2}>
                     <Text style = {styles.text}>목표 몸무게</Text>
-                    {props.isEditGoal? (
-                        <TextInput
-                        style = {styles.textInput}
-                        value = {props.tempGoal}
-                        onChangeText = {props.onChangeGoalText}
-                        onBlur = {props.onSaveGoal}
-                        keyboardType = 'numeric'/>
-                    ) : <Text style = {styles.textGoal}>{props.goal}</Text>}
-                    <Pressable
-                     onPress = {props.onChangeGoal}
-                     style = {styles.editButton}>
-                        <Text style = {styles.textButton}>수정</Text>
+                    <Pressable onPress = {props.onChangeGoal}>
+                        {props.isEditGoal? (
+                            <TextInput
+                            style = {styles.textInput}
+                            value = {props.tempGoal}
+                            onChangeText = {props.onChangeGoalText}
+                            onBlur = {props.onSaveGoal}
+                            keyboardType = 'numeric'/>
+                        ) : <Text style = {styles.textGoal}>{props.goal}</Text>}
                     </Pressable>
                 </View>
             </View>
@@ -129,9 +98,11 @@ const styles = StyleSheet.create({
     },
     GoalInnerWrapper1 : {
         justifyContent : 'center',
+        alignItems : 'center',
     },
     GoalInnerWrapper2 : {
         justifyContent : 'center',
+        alignItems : 'center',
     },
     EditWrapper : {
         flexDirection : 'row',
@@ -139,37 +110,21 @@ const styles = StyleSheet.create({
     },
     textInput : {
         fontSize : 15,
+        height : 34,
+        alignItems : 'center',
+        justifyContent : 'center',
     },
     textWeight : {
         color : 'black',
-        fontSize : 20,
+        fontSize : 25,
         fontWeight : '500',
         paddingHorizontal : 30,
     },
     textGoal : {
         color: 'black',
-        fontSize : 20,
+        fontSize : 25,
         fontWeight : '500',
         paddingHorizontal : 30,
-    },
-    editButton : {
-        alignItems : 'center',
-    },
-    editText : {
-        color : 'black',
-        fontWeight : '500',
-        backgroundColor : "#f3c315a2",
-        borderRadius : 5,
-    },
-    textButton : {
-        color : 'black',
-        fontSize : 14,
-        fontWeight : '400',
-        textAlign : 'center',
-        backgroundColor : "#f3c315a2",
-        borderRadius : 5,
-        paddingVertical : 2,
-        paddingHorizontal : 5,
     },
 })
 
