9f1c8463e0dc55dee8fb546fa20c7bd8ba180dee
CsLsj <lsj001018@naver.com>
2023-06-02 14:12:46 +0900

    fix ObesityDetailListComponent

--- src/components/ObesityDetailListComponent.tsx
+++ src/components/ObesityDetailListComponent.tsx
@@ -1,7 +1,10 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import Survey from './SurveyComponent';
 
 
 type ObesityDetailListProps = {
+    sidePicture : string;
+    upPicture : string;
     weight : string;
     obesity : number;
 }
@@ -9,25 +12,60 @@ type ObesityDetailListProps = {
 function ObesityDetailListComponent(props : ObesityDetailListProps){
     return(
         <View style = {styles.DetailWrapper}>
-            <View style = {styles.DetailDescriptionWrapper}>
-                <Text>몸무게</Text>
-                <Text>{props.weight}</Text>
+            <View style = {styles.PictureWrapper}>
+                {props.sidePicture ? <Image
+                    source = {{uri : props.sidePicture}}
+                    style = {styles.Image}/> : null}
+                {props.upPicture ?<Image
+                    source = {{uri : props.upPicture}}
+                    style = {styles.Image}/> : null}
             </View>
             <View style = {styles.DetailDescriptionWrapper}>
-                <Text>비만도</Text>
-                <Text>{props.obesity}</Text>
+                <View style = {styles.DetailDescriptionInnerWrapper}>
+                    <Text style = {styles.Description}>몸무게</Text>
+                    <Text style = {styles.Text}>{props.weight}</Text>
+                </View>
+                <View style = {styles.DetailDescriptionInnerWrapper}>
+                    <Text style = {styles.Description}>비만도</Text>
+                    <Text style = {styles.Text}>{props.obesity}</Text>
+                </View>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    PictureWrapper : {
+        flexDirection : 'row',
+    },
+    Image : {
+        width : 200,
+        height : 200
+    },
     DetailWrapper : {
 
     },
     DetailDescriptionWrapper : {
         flexDirection : 'row',
+        justifyContent : 'space-between',
     },
+    DetailDescriptionInnerWrapper : {
+        justifyContent : 'center',
+        alignItems : 'center',
+    },
+    Description : {
+        color : 'black',
+        fontSize : 20,
+        fontWeight : '600',
+    },
+    Text : {
+        color : 'black',
+        fontSize : 18,
+        fontWeight : '600',
+    },
+    SurveyWrapper : {
+
+    }
 })
 
 export default ObesityDetailListComponent;
