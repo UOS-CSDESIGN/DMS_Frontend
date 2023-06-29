b01cdc4908c863af3e59b3025f2ea5f3d61b6610
CsLsj <lsj001018@naver.com>
2023-06-29 20:34:03 +0900

    fix ObesityChartComponent

--- src/components/ObesityChartComponent.tsx
+++ src/components/ObesityChartComponent.tsx
@@ -3,6 +3,8 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {LineChart} from "react-native-chart-kit"
 import { StyleSheet } from "react-native";
 import { GestureResponderEvent } from "react-native-modal";
+import { Rect, Text as TextSVG, Svg } from "react-native-svg";
+import { useState } from "react";
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -13,10 +15,11 @@ interface ObesityChartProps {
 }
 
 function ObesityChart(props : ObesityChartProps){
+    let [tooltipPos, setTooltipPos] = useState({ x : 50, y :200 , visible : true, value : 0})
     const chartConfig = {
         backgroundColor: "white",
-        backgroundGradientFrom : "#eceae8",
-        backgroundGradientTo : "#919693",
+        backgroundGradientFrom : "#f7fcf7",
+        backgroundGradientTo : "#f7fcf7",
         decimalPlaces: 1, // 소수점 n째자리까지
         color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
         labelColor: (opacity = 0) => `rgba(0, 0, 0, ${opacity})`,
@@ -33,7 +36,46 @@ function ObesityChart(props : ObesityChartProps){
                     chartConfig = {chartConfig}
                     width = {width*0.9}
                     height = {height/2}
-                    />
+                    decorator={() => {
+                        return tooltipPos.visible? <View>
+                            <Svg>
+                                <Rect x = {tooltipPos.x-15}
+                                    y = {tooltipPos.y + 10}
+                                    width = "40"
+                                    height = "30"
+                                    fill = "white"/>
+                                <TextSVG
+                                    x = {tooltipPos.x + 5}
+                                    y = {tooltipPos.y + 30}
+                                    fill = "black"
+                                    fontSize = "16"
+                                    fontWeight = "bold"
+                                    textAnchor = "middle">
+                                    {tooltipPos.value.toFixed(1)}
+                                    </TextSVG>
+                                    {/*<TextSVG
+                                    x = {tooltipPos.x + 5}
+                                    y = {tooltipPos.y + 50}
+                                    fill = "black"
+                                    fontSize = "16"
+                                    fontWeight = "bold"
+                                    textAnchor = "middle">
+                                    {tooltipPos.obesity}
+                                </TextSVG> */}
+                            </Svg>
+                        </View> : null
+                    }}
+                    onDataPointClick={(data) => {
+                        let isSamePoint = (tooltipPos.x === data.x && tooltipPos.y === data.y);
+                        isSamePoint
+                          ? setTooltipPos((previousState) => ({
+                              ...previousState,
+                              value: data.value,
+                              visible: !previousState.visible,
+                            }))
+                          : setTooltipPos({ x: data.x, value: data.value, y: data.y, visible: true, });
+                      }}
+                />
             </View>
         </View>
     )
