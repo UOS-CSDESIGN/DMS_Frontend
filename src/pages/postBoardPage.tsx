36790dca9a2f9613a91074f66ec70e1574885095
CsLsj <lsj001018@naver.com>
2023-06-30 19:51:20 +0900

    fix Community

---
+++ src/pages/postBoardPage.tsx
@@ -0,0 +1,95 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { RootStackParamList } from "../AppInner"
+import {View, Text, Pressable, StyleSheet, SafeAreaView, FlatList} from "react-native"
+import { useCallback } from "react";
+import Icon from 'react-native-vector-icons/Ionicons'
+import { postType } from "../model/Community/Post";
+
+type PostBoardScreenProps = NativeStackScreenProps<RootStackParamList, 'PostBoardPage'>
+
+
+function PostBoardPage({navigation} : PostBoardScreenProps){
+    const boardData : postType = []; //게시판 data 여기로 가져오기
+    const data = [
+        { id : 1, title : '안녕', content : '반가워'},
+        { id : 2, title : '뭐해', content : '아무것도 안해'},
+        { id : 3, title : '그래', content : '그래'},
+        { id : 4, title : '오늘 뭐해', content : '글쎄'},
+    ]
+
+    const renderItem = ({item} : any) => (
+        <Pressable onPress = {toPost}>
+            <View style = {styles.PostDataWrapper}>
+                <Text style = {styles.PostDataTitle}>{item.title}</Text>
+                <Text style = {styles.PostDataText}>{item.content}</Text>
+                <View style = {styles.IconAndCountWrapper}>
+                    <Icon name = 'chatbox' size = {18}/>
+                    <Text style = {styles.CountText}>3</Text>
+                </View>
+            </View>
+        </Pressable>
+    )
+    const toPost = useCallback(() => {
+        navigation.navigate('PostPage')
+    },[navigation])
+
+    const keyExtractor = (item : any) => item.id.toString();
+
+    return(
+        <SafeAreaView style = {styles.PostBoardPage}>
+            <View style = {styles.PostBoardNameWrapper}>
+                <Text style = {styles.PostBoardNameText}>게시판</Text>
+            </View>
+            <View style = {styles.PostWrapper}>
+                <FlatList
+                    data = {data}
+                    renderItem = {renderItem}
+                    keyExtractor = {keyExtractor}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    PostBoardPage : {
+        backgroundColor : 'snow',
+        paddingVertical : '5%',
+        paddingHorizontal : '3%',
+    },
+    PostBoardNameWrapper : {
+        
+    },
+    PostBoardNameText : {
+        color : 'black',
+        fontSize : 16,
+        fontWeight : '600'
+    },
+    PostWrapper : {
+
+    },
+    PostDataWrapper : {
+        borderBottomWidth : 1,
+        paddingVertical : '1%',
+    },
+    PostDataTitle : {
+        color : 'black',
+        fontWeight : '600',
+        fontSize : 15,
+    },
+    PostDataText : {
+        color : 'black',
+        fontWeight : '500',
+        fontSize : 13,
+    },
+    IconAndCountWrapper : {
+        flexDirection : 'row',
+        alignItems : 'center',
+    },
+    CountText : {
+        paddingHorizontal : '2%',
+        paddingBottom : '0.8%'
+    }
+})
+
+export default PostBoardPage;
--- src/pages/postPage.tsx
+++ src/pages/postPage.tsx
@@ -1,15 +1,81 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { RootStackParamList } from "../AppInner"
-import {View, Text, Pressable, StyleSheet, ScrollView} from "react-native"
+import { View, Text, StyleSheet} from 'react-native'
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../AppInner";
+import Post from '../model/Community/Post';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 type PostScreenProps = NativeStackScreenProps<RootStackParamList, 'PostPage'>
 
-function PostPage(props : PostScreenProps){
+
+function PostPage({navigation} : PostScreenProps){
+    const postData : Post = [];
     return(
-        <View>
-            <Text>Hello</Text>
+        <View style = {styles.PostPage}>
+            <View>
+                <View style = {styles.PostNameWrapper}>
+                    <Text style = {styles.PostNameText}>게시판</Text>
+                </View>
+                <View style = {styles.PostInfoWrapper}>
+                    <Icon name = "person-circle-outline" size = {40} color = "gray"/>
+                    <View style = {styles.PostInfoInnerWrapper}>
+                        <Text style = {styles.PostInfoText}>이승주</Text>
+                        <Text style = {styles.PostInfoText}>2023/06/30 18:10</Text>
+                    </View>
+                </View>
+                <View style = {styles.PostWrapper}>
+                    <View style = {styles.PostInnerWrapper}>
+                        <Text style = {styles.PostTitleText}>치킨 뭐 먹을까</Text>
+                    </View>
+                    <Text style = {styles.PostDescriptionText}>간장치킨 vs 후라이드</Text>
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    PostPage : {
+        backgroundColor : 'snow',
+        paddingVertical : '5%',
+        paddingHorizontal : '3%',
+    },
+    PostNameWrapper : {
+        
+    },
+    PostNameText : {
+        color : 'black',
+        fontSize : 16,
+        fontWeight : '600'
+    },
+    PostInfoWrapper : {
+        paddingVertical : '3%',
+        flexDirection : 'row',
+        alignItems : 'center',
+    },
+    PostInfoInnerWrapper : {
+        paddingHorizontal : '1%',
+    },
+    PostInfoText : {
+        color : 'black',
+        fontWeight : '500',
+    },
+    PostWrapper : {
+        paddingHorizontal : '2.5%',
+        paddingVertical : '1%',
+    },
+    PostInnerWrapper : {
+        paddingVertical : '2%',
+    },
+    PostTitleText : {
+        color : 'black',
+        fontWeight : '600',
+        fontSize : 16,
+    },
+    PostDescriptionText : {
+        color : 'black',
+        fontWeight : '500',
+        fontSize : 14,
+    }
+})
 
 export default PostPage;
