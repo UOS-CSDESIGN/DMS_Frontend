b77dc01059a99994e6c03fb8aa59ac83c6e1f498
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-07-02 06:21:54 +0900

    fix: preview and board https get request

--- src/model/Community/getCategory.tsx
+++ src/model/Community/getCategory.tsx
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import Config from "react-native-config";
-import { getCategoryRequest, getCategorySuccess, getCategoryFailure } from "./slice/categorySlice";
+import { getBoardFailure, getBoardSuccess } from "./slice/boardSlice";
 
 async function getCategory(token:string, dispatch:any):Promise<any> {
     
     //const bearer = `Bearer ${JSON.parse(token)}`;
     const url = `${Config.SPRING_API}/board/list`;
 
-    dispatch(getCategoryRequest());
-    await axios.get(
-        url,
-    ).then((res) => {
-        dispatch(getCategorySuccess(res.data));
-        return Promise.resolve();
-    }).catch((error) => {
-        dispatch(getCategoryFailure());
+    await axios.get(url)
+        .then((res) => {
+            dispatch(getBoardSuccess(res.data));
+
+            return Promise.resolve();
+        }).
+        catch((error: AxiosError) => {
+            dispatch(getBoardFailure(error));
         
-        return Promise.reject();
-    });
+            return Promise.reject();
+        });
 }
 
 export default getCategory;
---
+++ src/model/Community/getPost.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import Config from "react-native-config";
+import { getPostFailure, getPostSuccess } from "./slice/postPreviewSlice";
+
+async function getPost(boardId:number,token: string|null, dispatch: any): Promise<any>{
+    
+    const url = `${Config.SPRING_API}/board/${boardId}/list`;
+
+    if (token == null) {
+        return Promise.reject();
+    }
+
+    await axios.get(
+        url,
+    ).then((res) => {
+        console.log(res.data);
+        dispatch(getPostSuccess(res.data));
+
+        return Promise.resolve();
+    }).catch((error) => {
+        console.log("error");
+        dispatch(getPostFailure(error));
+
+        return Promise.reject();
+    });
+}
+export default getPost;
