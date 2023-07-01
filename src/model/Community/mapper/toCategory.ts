4cd6f137f4f5b755f563698d99a6873aa4c80c02
seongho9 <91736237+seongho9@users.noreply.github.com>
2023-07-02 06:22:07 +0900

    remove mapper

--- src/model/Community/mapper/toCategory.ts
+++
@@ -1,8 +0,0 @@
-import Category from "../Category";
-
-function toCategory(boardId: number, boardName: string) {
-    return (
-        new Category(boardId, boardName)
-    );
-}
-export default toCategory;
